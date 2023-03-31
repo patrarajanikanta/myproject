f0772129e3be426d983f6a376b66c747ac50278e
patrarajanikanta <45660648+patrarajanikanta@users.noreply.github.com>
2023-03-31 15:51:03 +0530

    Update MyprojectApplication.java

--- myproject/src/main/java/com/devops/myproject/MyprojectApplication.java
+++ myproject/src/main/java/com/devops/myproject/MyprojectApplication.java
@@ -8,7 +8,7 @@ public class MyprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyprojectApplication.class, args);
-		System.out.println("Maven Test")
+		System.out.println("Maven Test");
 	}
 
 }
